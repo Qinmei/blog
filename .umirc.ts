2a52479ee294d4628253b542c9cb636445f5ec98
Qinmei <sun.houtao@outlook.com>
2021-02-20 11:09:32 +0800

    feature: add react 04

--- .umirc.ts
+++ .umirc.ts
@@ -29,7 +29,7 @@ export default defineConfig({
       },
       {
         title: 'React进阶',
-        children: ['blog/react/01', 'blog/react/02', 'blog/react/03'],
+        children: ['blog/react/01', 'blog/react/02', 'blog/react/03', 'blog/react/04'],
       },
       {
         title: 'React源码',
