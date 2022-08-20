e377ebbcb5353f0fa8d72930ae4561bdb00f20c5
Qinmei <16222151+Qinmei@users.noreply.github.com>
2022-08-21 03:16:35 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -5,12 +5,12 @@ const videos = `${assets}/videos`;
 const numArr = [...new Array(100).keys()].map(item => item.toString().padStart(3, '0'));
 
 export default {
-  favicon: `${images}/common/favicon.ico`,
-  logo: `${images}/common/logo.svg`,
+  favicon: `${images}/favicon.ico`,
+  logo: `${images}/logo.svg`,
   github: 'https://github.com/Qinmei',
   qinVideo: 'https://qinvideo.org',
-  bg: `${images}/common/bg.jpg`,
-  videobg: `${images}/common/video.jpg`,
+  bg: `${images}/bg.jpeg`,
+  videobg: `${images}/video.jpg`,
   reactPic: numArr.slice(1, 4).map(item => `${images}/react/code-${item}.jpg`),
   qinvideoPic: numArr.slice(1, 13).map(item => `${images}/qinvideo/demo-${item}.jpg`),
   qinplayerPic: numArr.slice(1, 6).map(item => `${images}/qinplayer/demo-${item}.png`),
